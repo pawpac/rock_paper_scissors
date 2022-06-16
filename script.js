0123a091aa6d1e2e5775d8a56d4906cfc166af28
Pawel Paczkowski <paw.pac@yahoo.com>
2022-06-16 18:47:31 -0500

    Build computerPlay function that returns string 'Rock', 'Paper', or 'Scissors'

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("rock paper scissors");
+function computerPlay () {
+  let move;
+  let result = Math.random().toFixed(1);
+  if (result > 0 && result <= 0.3) {
+      move = "Rock";
+  } else if (result > 0.3 && result <= 0.6) {
+      move = "Paper";
+  } else {
+      move = "Scissors";
+  }
+  return move;
+}
+
+let computerMove = computerPlay();
+console.log(computerMove);
