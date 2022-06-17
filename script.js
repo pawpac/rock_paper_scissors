5856b22e6589dabb0a30ceb7a4814bc8af162182
Pawel Paczkowski <paw.pac@yahoo.com>
2022-06-16 21:28:37 -0500

    Create function thta prompt user to make their choice and function that goes through single round of the game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Function that generates random pick
+
 function computerPlay () {
   let move;
   let result = Math.random().toFixed(1);
@@ -13,3 +15,35 @@ function computerPlay () {
 
 let computerMove = computerPlay();
 console.log(computerMove);
+
+
+// Function that prompt user to make their choice
+
+function playerSelection() {
+  let move = prompt("Rock, paper, or scissors? Make your move!");
+  return move;
+}
+
+let playerMove = playerSelection();
+console.log(playerMove);
+
+
+// Function that goes through single round of the game
+
+function singleRound(playerMove, computerMove) {
+  if (playerMove === "rock" && computerMove === "paper") {
+      return "computer";
+  } else if (playerMove === "rock" && computerMove === "scissors") {
+      return "player";
+  } else if (playerMove === "paper" && computerMove === "rock") {
+      return "player";
+  } else if (playerMove === "paper" && computerMove === "scissors") {
+      return "computer";
+  } else if (playerMove === "scissors" && computerMove === "rock") {
+      return "computer";
+  } else if (playerMove === "scissors" && computerMove === "paper") {
+      return "player";
+  } else {
+      return "tie";
+  }
+}
